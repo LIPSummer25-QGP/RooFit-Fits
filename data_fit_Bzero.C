c4edb904dab349b5c47266009a4d949dad387ad4
Ricardo Ribeiro <151793123+ricardo55ribeiro@users.noreply.github.com>
2025-08-22 16:46:35 +0100

    All MC Parameters Fixed

--- data_fit_Bzero.C
+++ data_fit_Bzero.C
@@ -23,6 +23,9 @@ using namespace RooFit;
 #include <RooExponential.h>
 #include <RooGenericPdf.h>  
 #include <RooProdPdf.h>
+#include <RooBifurGauss.h>
+#include <RooBinning.h>
+#include <RooChebychev.h>  
 
 #include <RooFitResult.h>
 #include <RooFit.h>
@@ -55,16 +58,32 @@ double getYatMass(RooPlot* frame, double mass) {
 void total_data_fit_Bd() {
     const int nbins_plot = 100; // Number of bins for the plotted data
 
-    double min_signal = 5.204911988;
-    double max_signal = 5.356368012;
+    double min_signal = 5.1;
+    double max_signal = 5.6;
 
-    double xlow = 5.1;
-    double xhigh = 5.5;
+    double xlow = 5.0;
+    double xhigh = 5.9;
     const double bin_width_plot = (xhigh - xlow) / nbins_plot;  // Used in y-axis label
+
+
+    double mc_mean = 5.28056;
+
+    double mc_sigma1_rt = 0.01344;
+    double mc_sigma2_rt = 0.03301;
+    double mc_c1_rt = 0.6584;
+
+    double mc_sigma1_wt = 0.07205;
+    double mc_sigma2_left_wt = 0.10056;
+    double mc_sigma2_right_wt = 0.23594;
+    double mc_c1_wt = 0.3179;
+
+    double mc_N_rt = 3239.96;
+    double mc_N_wt = 3156.04;
+    double mc_f1 = mc_N_rt / (mc_N_rt + mc_N_wt);
 
     
     // Load real data tree
-    TFile* file = TFile::Open("data_firstCut_Kstar_1108.root");
+    TFile* file = TFile::Open("data_unbinned_B0_FirstCut3.root");
     if (!file || file->IsZombie()) {
         std::cerr << "Error: Could not open real data file." << std::endl;
         return;
@@ -84,52 +103,92 @@ void total_data_fit_Bd() {
 
     RooDataSet dataset("dataset", "Unbinned dataset from TTree", tree, RooArgSet(B_mass));
 
-    // Signal: Double Gaussian
-    RooRealVar mean("mean", "Mean", 5.29, 5.275, 5.3);
+    // -------------------- Signal: RT (2 Gauss) + WT (Gauss + asymmetric Gauss) --------------------
+    // Shared mean (FIXED to MC)
+    RooRealVar mean("mean", "Mean", mc_mean);
+    mean.setConstant(true);
 
-    // Double Gaussian (shared mean)
-    RooRealVar sigma1("sigma1", "Sigma1", 0.009, 0.001, 0.08);
-    RooRealVar sigma2("sigma2", "Sigma2", 0.020, 0.001, 0.10);
-    RooRealVar c1("c1", "Fraction of Gauss1", 0.7, 0.0, 1.0);
 
-    RooGaussian gaus1("gaus1", "Gaussian 1", B_mass, mean, sigma1);
-    RooGaussian gaus2("gaus2", "Gaussian 2", B_mass, mean, sigma2);
+    // --- Right Tag (RT): two standard Gaussians, fixed sigmas and fixed fraction ---
+    RooRealVar sigma1_rt("sigma1_rt", "RT sigma1", mc_sigma1_rt);          
+    sigma1_rt.setConstant(true);
+    
+    RooRealVar sigma2_rt("sigma2_rt", "RT sigma2", mc_sigma2_rt);          
+    sigma2_rt.setConstant(true);
+    
+    RooRealVar c1_rt("c1_rt", "RT fraction of G1", mc_c1_rt);      
+    c1_rt.setConstant(true);
 
-    // Signal shape becomes a sum of the two Gaussians
-    RooAddPdf signal("signal", "Double Gaussian", RooArgList(gaus1, gaus2), RooArgList(c1));
+    RooGaussian rt_g1("rt_g1", "RT Gaussian 1", B_mass, mean, sigma1_rt);
+    RooGaussian rt_g2("rt_g2", "RT Gaussian 2", B_mass, mean, sigma2_rt);
+    RooAddPdf   rt_pdf("rt_pdf", "RT: two Gaussians", RooArgList(rt_g1, rt_g2), RooArgList(c1_rt));
 
+
+    // --- Wrong Tag (WT): one standard Gaussian + one asymmetric Gaussian (bifurcated), fixed params ---
+    RooRealVar sigma1_wt("sigma1_wt", "WT sigma1 (Gauss)", mc_sigma1_wt);        
+    sigma1_wt.setConstant(true);
+
+    RooRealVar sigma2L_wt("sigma2L_wt", "WT sigma2 left (bif)", mc_sigma2_left_wt);
+    sigma2L_wt.setConstant(true);
+
+    RooRealVar sigma2R_wt("sigma2R_wt", "WT sigma2 right (bif)", mc_sigma2_right_wt);  
+    sigma2R_wt.setConstant(true);
+
+    RooRealVar c1_wt("c1_wt","WT fraction of Gauss1",mc_c1_wt);            
+    c1_wt.setConstant(true);
+
+
+    RooGaussian wt_g1("wt_g1", "WT Gaussian 1", B_mass, mean, sigma1_wt);
+    RooBifurGauss wt_g2("wt_g2", "WT asymmetric Gaussian", B_mass, mean, sigma2L_wt, sigma2R_wt);
+    RooAddPdf wt_pdf("wt_pdf", "WT: Gauss + bifurGauss", RooArgList(wt_g1, wt_g2), RooArgList(c1_wt));
+
+
+    // --- RT/WT mixture with floating fraction f1 (RT weight) ---
+    RooRealVar f1("f1", "Fraction of RT in signal", mc_f1);  
+    f1.setConstant(true);
+
+    // Full signal shape (normalized): f1 * RT  +  (1-f1) * WT
+    RooAddPdf signal("signal", "Signal = f1*RT + (1-f1)*WT", RooArgList(rt_pdf, wt_pdf), RooArgList(f1));
 
     RooRealVar Nsig("Nsig", "Signal Yield", 261, 0, 300000);
     RooExtendPdf signal_ext("signal_ext", "Extended Signal", signal, Nsig);
 
+
+    /*
     // Background: Exponential model
     RooRealVar lambda("lambda", "Lambda", -2.72, -6.0, -0.1);
     RooExponential expo("expo", "Background", B_mass, lambda);
 
-    //RooRealVar c0("c0", "c0", 1.0, -1e5, 1e5);
-    //RooRealVar c1("c1", "c1", 0.0, -1e5, 1e5);
-    //RooRealVar c2("c2", "c2", 0.0, -1e5, 1e5);
-    //RooRealVar c3("c3", "c3", 0.0, -1e5, 1e5);
-    //RooRealVar c4("c4", "c4", 0.0, -1e5, 1e5);
-    //RooPolynomial poly("poly", "4th-degree polynomial background", B_mass, RooArgList(c0, c1, c2, c3, c4));
-
     RooRealVar Nbkg("Nbkg", "Background Yield", 1614, 0, 1700000);
     RooExtendPdf expo_ext("expo_ext", "Extended Exponential Background", expo, Nbkg);
+    */
+
+
+    RooRealVar a0("a0","a0",  0.0, -2.0, 2.0);
+    RooRealVar a1("a1","a1",  0.0, -2.0, 2.0);
+    RooRealVar a2("a2","a2",  0.0, -2.0, 2.0);
+    RooRealVar a3("a3","a3",  0.0, -2.0, 2.0);
+    RooRealVar a4("a4","a4",  0.0, -2.0, 2.0);
+    RooChebychev bkg_pdf("bkg_pdf","Cheb4", B_mass, RooArgList(a0,a1,a2,a3,a4));
+
+    RooRealVar Nbkg("Nbkg","Background Yield", 1.5e6, 0, 1e8);
+    RooExtendPdf bkg_ext("bkg_ext","Extended background", bkg_pdf, Nbkg);
 
     // Combined model
-    RooAddPdf model("model", "Signal + Background", RooArgList(signal_ext, expo_ext));
+    RooAddPdf model("model", "Signal + Background", RooArgList(signal_ext, bkg_ext));
 
     // Fit (Extended Maximum Likelihood Method)
     RooFitResult* result = model.fitTo(dataset, Save());
 
+    
     // Compute background-only integrals in signal and sideband regions
     B_mass.setRange("signalRegion", min_signal, max_signal);
     B_mass.setRange("lowSideband", xlow, min_signal);
     B_mass.setRange("highSideband", max_signal, xhigh);
 
-    double frac_bkg_signal = expo.createIntegral(B_mass, NormSet(B_mass), Range("signalRegion"))->getVal(); // Background in Signal Region
-    double frac_bkg_low    = expo.createIntegral(B_mass, NormSet(B_mass), Range("lowSideband"))->getVal(); // Background in Left Noise Region
-    double frac_bkg_high   = expo.createIntegral(B_mass, NormSet(B_mass), Range("highSideband"))->getVal(); // Background in Right Noise Region
+    double frac_bkg_signal = bkg_pdf.createIntegral(B_mass, NormSet(B_mass), Range("signalRegion"))->getVal(); // Background in Signal Region
+    double frac_bkg_low    = bkg_pdf.createIntegral(B_mass, NormSet(B_mass), Range("lowSideband"))->getVal(); // Background in Left Noise Region
+    double frac_bkg_high   = bkg_pdf.createIntegral(B_mass, NormSet(B_mass), Range("highSideband"))->getVal(); // Background in Right Noise Region
 
     double total_bkg_yield = Nbkg.getVal(); // Total background
 
@@ -157,7 +216,7 @@ void total_data_fit_Bd() {
     }
 
     // Apply same cuts
-    TString cut_mc = Form("Bchi2cl>0.003 && Bnorm_svpvDistance_2D>3.9916 && (%s) && (%s) && (%s) && (%s)",
+    TString cut_mc = Form("Bnorm_svpvDistance_2D>2.5177 && ((%s)||(Bgen==41000)) && (%s) && (%s) && (%s)",
                         isMCsignal.Data(),
                         ACCcuts_ppRef.Data(),
                         SELcuts_ppRef.Data(),
@@ -192,11 +251,21 @@ void total_data_fit_Bd() {
 
     RooPlot* frame = B_mass.frame(Range(xlow, xhigh), Bins(nbins_plot));
 
-    // Plot data + model with the same naming/styles used for pulls
+
+
     dataset.plotOn(frame, Binning(B_mass.getBinning("mainBins")), MarkerStyle(20), MarkerSize(1.2), Name("data"), DataError(RooAbsData::Poisson));
-    model.plotOn(frame, LineColor(kBlue), LineWidth(2), Name("global"));  // Total model
-    model.plotOn(frame, Components(expo_ext), LineColor(kRed), LineStyle(kDashed), LineWidth(2), Name("background"));
-    model.plotOn(frame, Components(signal), LineColor(kGreen + 2), LineStyle(kDashed), LineWidth(2), Name("signal"));
+
+    // Background component
+    model.plotOn(frame, Components(bkg_ext), LineColor(kRed), LineStyle(kDashed), LineWidth(2), Name("background"));
+
+    // RT and WT components (shape-only subcomponents of the signal)
+    model.plotOn(frame, Components(rt_pdf), LineColor(kGreen + 2), LineStyle(kDashed), LineWidth(2), Name("RT"));
+    model.plotOn(frame, Components(wt_pdf), LineColor(kMagenta + 2), LineStyle(kDashed), LineWidth(2), Name("WT"));
+
+    // Total model
+    model.plotOn(frame, LineColor(kBlue), LineWidth(2), Name("global"));
+
+
 
     frame->SetTitle("");
     frame->GetYaxis()->SetTitleOffset(1.5);
@@ -224,41 +293,58 @@ void total_data_fit_Bd() {
 
     // ---------- Legend (same place), on TOP pad ----------
     p1->cd();
-    TLegend* legend = new TLegend(0.58, 0.66, 0.88, 0.88);
+    TLegend* legend = new TLegend(0.48, 0.77, 0.88, 0.98);
     legend->SetTextFont(42);
     legend->SetTextSize(0.025);
     legend->SetBorderSize(1);
     legend->SetLineColor(kBlack);
-    legend->SetFillStyle(0);
+    legend->SetFillStyle(1001);
+    legend->SetFillColor(kWhite);
     legend->AddEntry(frame->findObject("data"), "Data (B^{0} )", "lep");
-    legend->AddEntry(frame->findObject("background"), "Background Fit (Exponential)", "l");
-    legend->AddEntry(frame->findObject("signal"), "Signal Fit (Double Gaussian)", "l");
-    legend->AddEntry(frame->findObject("global"), "Signal + Background Fit", "l");
+    legend->AddEntry(frame->findObject("background"), "Background (4th degree poly)", "l");
+    legend->AddEntry(frame->findObject("RT"),         "RT Signal", "l");
+    legend->AddEntry(frame->findObject("WT"),         "WT Signal", "l");
+    legend->AddEntry(frame->findObject("global"),     "Total Fit", "l");
     legend->Draw();
+
 
     // ---------- TPaveText (same place), on TOP pad ----------
     p1->cd();
-    TPaveText* pave = new TPaveText(0.63, 0.38, 0.88, 0.66, "NDC");
+    TPaveText* pave = new TPaveText(0.63, 0.22, 0.88, 0.77, "NDC");
     pave->SetTextAlign(12);
     pave->SetTextFont(42);
     pave->SetTextSize(0.025);
     pave->SetFillColor(0);
     pave->SetBorderSize(1);
-    pave->AddText(Form("Mean = %.5f #pm %.5f", mean.getVal(), mean.getError()));
 
-    pave->AddText(Form("#sigma_{1} = %.5f #pm %.5f", sigma1.getVal(), sigma1.getError()));
-    pave->AddText(Form("#sigma_{2} = %.5f #pm %.5f", sigma2.getVal(), sigma2.getError()));
-    pave->AddText(Form("c1 = %.3f #pm %.3f", c1.getVal(), c1.getError()));
+    
+    pave->AddText(Form("Mean (fixed) = %.5f", mean.getVal()));
+    // RT fixed params
+    pave->AddText(Form("#sigma_{1,RT} (fixed) = %.5f", sigma1_rt.getVal()));
+    pave->AddText(Form("#sigma_{2,RT} (fixed) = %.5f", sigma2_rt.getVal()));
+    pave->AddText(Form("c_{1,RT} (fixed) = %.3f", c1_rt.getVal()));
+    // WT fixed params
+    pave->AddText(Form("#sigma_{1,WT} (fixed) = %.5f", sigma1_wt.getVal()));
+    pave->AddText(Form("#sigma_{2L,WT} (fixed) = %.5f", sigma2L_wt.getVal()));
+    pave->AddText(Form("#sigma_{2R,WT} (fixed) = %.5f", sigma2R_wt.getVal()));
+    pave->AddText(Form("c_{1,WT} (fixed) = %.3f", c1_wt.getVal()));
+    // Floating RT/WT mixture
+    pave->AddText(Form("f_{1} (fixed) = %.3f", f1.getVal()));
 
     pave->AddText(Form("N_{sig} = %.1f #pm %.1f", Nsig.getVal(), Nsig.getError()));
-    pave->AddText(Form("#lambda = %.5f #pm %.5f", lambda.getVal(), lambda.getError()));
+    pave->AddText(Form("a_{0} = %.4f #pm %.4f", a0.getVal(), a0.getError()));
+    pave->AddText(Form("a_{1} = %.4f #pm %.4f", a1.getVal(), a1.getError()));
+    pave->AddText(Form("a_{2} = %.4f #pm %.4f", a2.getVal(), a2.getError()));
+    pave->AddText(Form("a_{3} = %.4f #pm %.4f", a3.getVal(), a3.getError()));
+    pave->AddText(Form("a_{4} = %.4f #pm %.4f", a4.getVal(), a4.getError()));
     pave->AddText(Form("N_{bkg} = %.1f #pm %.1f", Nbkg.getVal(), Nbkg.getError()));
-    pave->AddText(Form("#chi^{2}/ndf = %.2f", chi2));
+    pave->AddText(Form("#chi^{2}/ndf = %.5f", chi2));
     pave->Draw();
+    
 
     // ---------- f_b / f_s box (same place), on TOP pad ----------
     p1->cd();
-    TPaveText* pave_fb_fs = new TPaveText(0.46, 0.77, 0.58, 0.88, "NDC");
+    TPaveText* pave_fb_fs = new TPaveText(0.36, 0.87, 0.48, 0.98, "NDC");
     pave_fb_fs->SetTextAlign(12);
     pave_fb_fs->SetTextFont(42);
     pave_fb_fs->SetTextSize(0.025);
@@ -302,8 +388,8 @@ void total_data_fit_Bd() {
     zeroLine->Draw("same");
 
 
-
-    c->SaveAs("Bd_Total_Fit_With_Pulls.pdf");
+    TString name_of_the_file = "Bd_Total_Fit_RT_and_WT.pdf";
+    c->SaveAs(name_of_the_file);
 
     std::cout << std::fixed << std::setprecision(2);
     std::cout << " " << std::endl;
@@ -318,7 +404,7 @@ void total_data_fit_Bd() {
     std::cout << "f_{s} = " << f_s << std::endl;
     std::cout << " " << std::endl; 
 
-    std::cout << "Gaussian + Exponential fit complete. Output saved to 'Bd_Total_Fit_With_Pulls.pdf'" << std::endl;
+    std::cout << "Fit saved to " << name_of_the_file << std::endl;
 
     delete c;
     delete tree;
